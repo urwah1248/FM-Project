42d3aa9c8a7076cc3cd03802ca48549d241095ad
M Urwah Shahid <urwahpatel@gmail.com>
2023-01-10 23:15:36 +0500

    fixed static api

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ app.get('/products', (req, res) => {
 
 app.get('/products/:id', (req, res) => {
     const id  = req.params.id
-    res.json(products[id])
+    res.json(products[id-1])
 })
 
-app.get('/products/:type', (req, res) => {
+app.get('/products/type/:type', (req, res) => {
     const type  = req.params.type
     const filtered = products.filter(product => {
-        
+        return type===product.type
     })
     res.json(filtered)
 })
